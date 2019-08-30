834d712d85654e29b4c8b2b2e9476b2b394061b8
James Fong <jfong2@stevens.edu>
2019-08-30 16:51:35 -0400

    homework 1 complete

---
+++ loopcode.cc
@@ -0,0 +1,17 @@
+//HW1 - Loops program
+//Author: James Fong
+//I pledge my honor that I have abided by the stevens honor system.
+//Cite: Peter Ho
+
+#include <iostream>
+using namespace std;
+
+int main(){
+  //  for (initial assignments; not-stopping-conditions; end-of-loop-things)
+  
+  for (int i = 0; i <= 10; i += 1)
+    cout << i << endl;
+  
+  return 0;
+
+}
